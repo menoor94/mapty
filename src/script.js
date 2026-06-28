459f84195ceef5275721d9b31827e3bb191ed73c
Menoor <noorpazhouhm@gmail.com>
2026-06-28 19:10:55 +0330

    Refactoring the code in OOP

--- src/script.js
+++ src/script.js
@@ -9,64 +9,83 @@ const workoutType = document.getElementById("workout-type");
 const workoutCadence = document.getElementById("workout-cadence");
 const workoutDuration = document.getElementById("workout-duration");
 const workoutElev = document.getElementById("workout-elev");
-
 let mapEvent, map;
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    (pos) => {
-      const { latitude } = pos.coords;
-      const { longitude } = pos.coords;
-      const coords = [latitude, longitude];
 
-      map = L.map("map").setView(coords, 15);
+class App {
+  #map;
+  #mapEvent;
 
-      L.tileLayer(
-        "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
-        {
-          attribution:
-            "&copy; OpenStreetMap contributors, Tiles style by CyclOSM hosted by OpenStreetMap France",
+  constructor() {
+    this._getPosition();
+    form.addEventListener("submit", this._newWorkout.bind(this));
+    workoutType.addEventListener("change", this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          console.error("could not do it");
         },
-      ).addTo(map);
+      );
+    }
+  }
 
-      map.on("click", function (e) {
-        mapEvent = e;
-        formContainer.classList.remove("hidden");
-        distance.focus();
-      });
-    },
-    () => {
-      console.error("could not do it");
-    },
-  );
+  _loadMap(pos) {
+    const { latitude } = pos.coords;
+    const { longitude } = pos.coords;
+    const coords = [latitude, longitude];
+
+    this.#map = L.map("map").setView(coords, 15);
+
+    L.tileLayer(
+      "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
+      {
+        attribution:
+          "&copy; OpenStreetMap contributors, Tiles style by CyclOSM hosted by OpenStreetMap France",
+      },
+    ).addTo(this.#map);
+
+    this.#map.on("click", this._showForm.bind(this));
+  }
+  _showForm(e) {
+    this.#mapEvent = e;
+    formContainer.classList.remove("hidden");
+    distance.focus();
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    workoutType.value = "Running";
+    workoutCadence.value = workoutDuration.value = distance.value = "";
+
+    let type;
+    workoutType.value === "Running"
+      ? (type = "running-popup")
+      : (type = "cycling-popup");
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          minWidth: 90,
+          maxWidth: 150,
+          autoClose: false,
+          closeOnClick: false,
+          className: type,
+        }),
+      )
+      .setPopupContent(workoutType.value)
+      .openPopup();
+  }
+
+  _toggleElevationField() {
+    workoutCadence.closest(".form--row").classList.toggle("hidden");
+    workoutElev.closest(".form--row").classList.toggle("hidden");
+  }
 }
-console.log(form);
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
 
-  workoutType.value = "Running";
-  workoutCadence.value = workoutDuration.value = distance.value = "";
-
-  let type;
-  workoutType.value === "Running"
-    ? (type = "running-popup")
-    : (type = "cycling-popup");
-
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        minWidth: 90,
-        maxWidth: 150,
-        autoClose: false,
-        closeOnClick: false,
-        className: type,
-      }),
-    )
-    .setPopupContent(workoutType.value)
-    .openPopup();
-});
-workoutType.addEventListener("change", function () {
-  workoutCadence.closest(".form--row").classList.toggle("hidden");
-  workoutElev.closest(".form--row").classList.toggle("hidden");
-});
+const app = new App();
